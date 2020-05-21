1463e65b86ab8200e3dd390162315c7b47296da3
Samyak Srivastava <samyaksrivastava9@gmail.com>
2020-05-21 19:22:42 +0530

    Create program5.py

---
+++ day1/program5.py
@@ -0,0 +1,18 @@
+r1=int(input())
+r2=int(input())
+r3=int(input())
+sr1=(r1*100.0)/60
+sr2=(r2*100.0)/60
+sr3=(r3*100.0)/60
+R1=r1*2
+R2=r2*2
+R3=r3*2
+print("Strike rate of player 1:",sr1)
+print("Strike rate of player 2:",sr2)
+print("Strike rate of player 3:",sr3)
+print("Runs scored by player 1 on playing 60 more balls: ",R1)
+print("Runs scored by player 2 on playing 60 more balls: ",R2)
+print("Runs scored by player 3 on playing 60 more balls: ",R3)
+print("Maximum number of sixes that player 1 can hit: ",r1//6)
+print("Maximum number of sixes that player 2 can hit: ",r2//6)
+print("Maximum number of sixes that player 3 can hit: ",r3//6)
