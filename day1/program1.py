9b5dcaa27f8002b75ff3e0d9977f4d19ff7135d6
Samyak Srivastava <samyaksrivastava9@gmail.com>
2020-05-21 19:15:17 +0530

    Create program1.py

---
+++ day1/program1.py
@@ -0,0 +1,11 @@
+
+name=("Samyak")
+branch=("CSE")
+age=21
+college=("HBTU Kanpur")
+gender=("Male")
+print("Name: ", name)
+print("Branch: ",branch)
+print("Age: ", age)
+print("College name: ", college)
+print("Gender: ",gender)
