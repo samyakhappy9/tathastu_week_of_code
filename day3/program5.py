2b15e238fa7cf117e93f10b242ee74d0d8edc7e4
Samyak Srivastava <samyaksrivastava9@gmail.com>
2020-05-23 15:21:01 +0530

    Create program5.py

---
+++ day3/program5.py
@@ -0,0 +1,18 @@
+le1=int(input())
+le2=int(input())
+li1=[]
+li2=[]
+for i in range(le1):
+     n=int(input())
+     li1.append(n)
+
+for i in range(le2):
+     n=int(input())
+     li2.append(n)
+
+for i in li1:
+     if i not in li2:
+         pass
+     else:
+         print(i)
+         
