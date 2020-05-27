88e1cfc504c9f664b3b16e72db9cb8bbc4e6bf62
Samyak Srivastava <samyaksrivastava9@gmail.com>
2020-05-27 15:51:08 +0530

    Update program6.py

--- day6/program6.py
+++ day6/program6.py
@@ -1,31 +1,30 @@
 import sys  
-def check(arr, n): 
-	  minEle = sys.maxsize 
-	  maxEle = -sys.maxsize - 1
-	  minIndex = -1
- 
-	  for i in range(n): 
-		      if arr[i]< minEle: 
-			        minEle = arr[i] 
-			        minIndex = i
-	  flag1 = 1
-	  for i in range(1, minIndex): 
-		      if arr[i] < arr[i - 1]: 
-			        flag1 = 0
-			        break
-	  flag2 = 1
-	  for i in range(minIndex + 1, n) : 
-		      if arr[i] < arr[i - 1]: 
-			        flag2 = 0
-			        break
-    if flag1+flag2 ==2 and arr[n-1]<arr[minIndex - 1]: 
-		      print("YES") 
-	  else: 
-		      print("NO") 
+def check(l,n): 
+      min= sys.maxsize 
+      max= -sys.maxsize - 1
+      index = -1
+      for i in range(n): 
+	      if l[i]< min: 
+		      min=l[i] 
+		      index=i
+      f1 = 1
+      for i in range(1,index): 
+              if l[i] < l[i-1]: 
+		      f1 = 0
+		      break
+      f2 = 1
+      for i in range(index + 1,n) : 
+	      if l[i] < l[i-1]: 
+		      f2 = 0
+		      break
+      if f1+f2 ==2 and l[n-1] < l[index-1]: 
+	        print("YES") 
+      else: 
+		print("NO") 
 
 n=int(input())
 l=[]
 for i in range(n):
-    x=int(input())
-    l.append(x)
+      x=int(input())
+      l.append(x)
 print(check(l, n)) 
