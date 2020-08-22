970f14b9b0b96018300ad298d83ce6ccb1117ce8
Samyak Srivastava <samyaksrivastava9@gmail.com>
2020-08-22 19:37:16 +0530

    Update program5.py

--- day4/program5.py
+++ day4/program5.py
@@ -1,14 +1,13 @@
 n=int(input())
 l=[]
 for i in range(n):
-     s=str(input())
-     l.append(s)
+    s=str(input())
+    l.append(s)
 name=l[0]
 max=0
 for i in l:
-     if l.count(i)>=max:
-         max=l.count(i)
-         if i<=name:
-             name=i
-             
-print(smname)
+    if l.count(i)>=max:
+        max=l.count(i)
+        if i<=name:
+            name=i       
+print(name)
