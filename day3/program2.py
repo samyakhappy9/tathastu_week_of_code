299a3495e7f099c6f6bd66fe3259672d22d91097
Samyak Srivastava <samyaksrivastava9@gmail.com>
2020-08-22 17:47:09 +0530

    Update program2.py

--- day3/program2.py
+++ day3/program2.py
@@ -1,9 +1,7 @@
 from itertools import permutations
-import string
 s=str(input())
 l=permutations(s)
-for i in l: 
-     for j in i:
-         print(j,end='')
-     print('')    
+for i in l:
+     print(''.join(i))
+         
      
